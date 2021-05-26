9a4f6294e075496e41b2110765913223736c6dc5
Robin <faisalrobin22@gmail.com>
2021-05-27 00:23:05 +0600

    pms bd

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bgimg {
+  background-image: url('https://pms-bd.com/shipping_port.jpg');
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: white;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
+
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
+
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
+
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+hr {
+  margin: auto;
+  width: 40%;
+}
+</style>
+<body>
+
+<div class="bgimg">
+  <div class="topleft">
+    <p style="color:#000">PMS BD</p>
+  </div>
+  <div class="middle">
+    <h1 style="color:#000">COMING SOON</h1>
+  </div>
+  <div class="bottomleft">
+    <p>Stay With Us</p>
+  </div>
+</div>
+
+</body>
+</html>
